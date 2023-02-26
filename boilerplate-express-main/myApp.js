b238bb15368e37b2ec599ada7af9f555f76aefa1
partially-unresponsive <unsecuredmail@proton.me>
2023-02-26 17:54:58 +0200

    feat: Added server request to /json

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -3,6 +3,9 @@ let app = express();
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
+app.get("/json", (req, res) => {
+    res.json({"message": "Hello json"});
+});
 app.use("/public", express.static(__dirname + "/public"));
 console.log('Hello World');
 
